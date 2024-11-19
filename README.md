f9410c08c1f8b451c918184875005cb69ed650fb
PotoBW <dmosquerabw@gmail.com>
2024-11-19 14:29:25 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# ShaguScore_Complete_Spanish
+
+Este complemento es una clasificación de objetos similar a GearScore. Incluye una pequeña base de datos de los niveles de todos los objetos disponibles en la versión clásica hasta Naxxramas. Esos valores se imprimen como puntuación, de forma similar a GearScore. No te tomes esos valores demasiado en serio, en la versión original el nivel de objeto no vale nada.
+
+# Traducción por PotoBW
+
+Este addon fue desarrollado por Shagu. Nosotros solo traducimos para que este completamente en español. Está basado en la versión ShaguScore (2.0). En caso de haber errores de traducción o errores de textos que no se entienden contactenme para solucionarlos.
+
+Telefono, WhatSapp, Telegram: +53 55920042
+
+Telegram: @potobw
+
+Correo: dmosquerabw@gmail.com
+
+# Donaciones
+
+PayPal: joseamosher@gmail.com
+
+![character](https://raw.githubusercontent.com/shagu/ShaguAddons/master/_img/ShaguScore/character.jpg)
+
+## Installation
+1. Descargar **[Latest Version](https://github.com/shagu/ShaguScore/archive/master.zip)**
+2. Descomprima el archivo RAR
+3. Copiar "ShaguScore" en Directorio-del-WoW\Interface\AddOns
+4. Reiniciar Wow
+
+El valor de la puntuación se ha calculado de la siguiente manera:
+
+    Puntaje = Rareza * ItemNivel * Bonificación
+
+**Rareza:**
+<span style="color: #9d9d9d">Pobre (0)</span>,
+<span style="color: #000000">Común (1)</span>,
+<span style="color: #1eff00">Poco común (2)</span>,
+<span style="color: #0080ff">Extraño (3)</span>,
+<span style="color: #b048f8">Épico (4)</span>,
+<span style="color: #ff8000">Legendario (5)</span>,
+<span style="color: #e6cc80">Artefacto (6)</span>
+
+**Bonificación:** 
+La bonificación es 2 cuando se usa un arma de dos manos, de lo contrario será 1.
---
+++ ShaguScore.lua
@@ -0,0 +1,252 @@
+ShaguScore = CreateFrame( "Frame" , "ShaguScoreTooltip", GameTooltip )
+ShaguScore:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+ShaguScore:SetScript("OnEvent", function()
+  score, r, g, b = ShaguScore:ScanUnit("mouseover")
+  if score and r and g and b then
+    GameTooltip:AddLine("ShaguPuntuación: " .. score, r,g,b)
+    GameTooltip:Show()
+  end
+end)
+
+ShaguScore:SetScript("OnShow", function()
+  if GameTooltip.itemLink then
+    local _, _, itemID = string.find(GameTooltip.itemLink, "item:(%d+):%d+:%d+:%d+")
+    local _, _, itemLink = string.find(GameTooltip.itemLink, "(item:%d+:%d+:%d+:%d+)");
+
+    if not itemLink then return end
+
+    local itemLevel = ShaguScore.Database[tonumber(itemID)] or 0
+    local _, _, itemRarity, _, _, _, _, itemSlot, _ = GetItemInfo(itemLink)
+    local r,g,b = GetItemQualityColor(itemRarity)
+
+    local score = ShaguScore:Calculate(itemSlot, itemRarity, itemLevel)
+    if score and score > 0 then
+      GameTooltip:AddLine("ShaguPuntuación: " .. score, r, g, b)
+      GameTooltip:Show()
+    end
+  end
+end)
+
+ShaguScore:SetScript("OnHide", function()
+  GameTooltip.itemLink = nil
+end)
+
+local function GetItemLinkByName(name)
+  for itemID = 1, 65536 do
+    local itemName, hyperLink, itemQuality = GetItemInfo(itemID)
+    if (itemName and itemName == name) then
+      local _, _, _, hex = GetItemQualityColor(tonumber(itemQuality))
+      return hex.. "|H"..hyperLink.."|h["..itemName.."]|h|r"
+    end
+  end
+end
+
+-- target inspect
+ShaguScoreHookInspectUnit = InspectUnit
+function InspectUnit(unit)
+  ShaguScoreHookInspectUnit(unit)
+  ShaguScore.Inspect = ShaguScore.Inspect or CreateFrame("Frame", nil, InspectModelFrame)
+  ShaguScore.Inspect:SetFrameStrata("HIGH")
+  ShaguScore.Inspect:SetWidth(200)
+  ShaguScore.Inspect:SetHeight(25)
+  ShaguScore.Inspect:SetPoint("BOTTOM", 0, 0)
+  ShaguScore.Inspect.text = ShaguScore.Inspect.text or ShaguScore.Inspect:CreateFontString("Status", "TOOLTIP", "GameFontNormal")
+  ShaguScore.Inspect.text:SetPoint("CENTER", 0, 0)
+
+  local score, r, g, b = ShaguScore:ScanUnit("target")
+  if score and r and g and b then
+    ShaguScore.Inspect.text:SetText("ShaguPuntuación: " .. score)
+    ShaguScore.Inspect.text:SetTextColor(r, g, b)
+  end
+end
+
+-- player inspect
+ShaguScore.CharacterFrame = CreateFrame("Frame", nil, CharacterModelFrame)
+ShaguScore.CharacterFrame:SetFrameStrata("HIGH")
+ShaguScore.CharacterFrame:SetWidth(200)
+ShaguScore.CharacterFrame:SetHeight(50)
+ShaguScore.CharacterFrame:SetPoint("BOTTOM", 0, 0)
+ShaguScore.CharacterFrame.text = ShaguScore.CharacterFrame:CreateFontString("Status", "LOW", "GameFontNormal")
+ShaguScore.CharacterFrame.text:SetPoint("CENTER", 0, 0)
+
+ShaguScore.CharacterFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+ShaguScore.CharacterFrame:RegisterEvent("UNIT_NAME_UPDATE")
+ShaguScore.CharacterFrame:RegisterEvent("UNIT_PORTRAIT_UPDATE")
+ShaguScore.CharacterFrame:RegisterEvent("BAG_UPDATE")
+ShaguScore.CharacterFrame:SetScript("OnEvent", function()
+  score, r, g, b = ShaguScore:ScanUnit("player")
+  if score and r and g and b then
+    ShaguScore.CharacterFrame.text:SetText("ShaguPuntuación: " .. score)
+    ShaguScore.CharacterFrame.text:SetTextColor(r, g, b)
+  end
+end)
+
+-- functions
+function ShaguScore:round(input, places)
+  if not places then places = 0 end
+  if type(input) == "number" and type(places) == "number" then
+    local pow = 1
+    for i = 1, places do pow = pow * 10 end
+    return floor(input * pow + 0.5) / pow
+  end
+end
+
+function ShaguScore:Calculate(slot, rarity, ilvl)
+  if not rarity then return nil end
+  return (rarity * ilvl) * (slot == "INVTYPE_2HWEAPON" and 2 or 1)
+end
+
+function ShaguScore:ScanUnit(target)
+  if not UnitIsPlayer(target) then return nil end
+
+  local count, ar, ag, ab, score = 0, 0, 0, 0, 0
+
+  for i=1,19 do
+    if GetInventoryItemLink(target, i) then
+      local _, _, itemID = string.find(GetInventoryItemLink(target, i), "item:(%d+):%d+:%d+:%d+")
+      local _, _, itemLink = string.find(GetInventoryItemLink(target, i), "(item:%d+:%d+:%d+:%d+)");
+
+      local itemLevel = ShaguScore.Database[tonumber(itemID)] or 0
+      local _, _, itemRarity, _, _, _, _, itemSlot, _ = GetItemInfo(itemLink)
+      local r, g, b = .2, .2, .2
+
+      local cscore = 0
+
+      if itemRarity and itemSlot then
+        r,g,b, _ = GetItemQualityColor(itemRarity)
+        ar = ar + r ; ag = ag + g ; ab = ab + b
+        cscore = ShaguScore:Calculate(itemSlot, itemRarity, itemLevel)
+      end
+
+      score = score + cscore
+      count = count + 1
+    end
+  end
+
+  local ar = ShaguScore:round(ar / count, 2);
+  local ag = ShaguScore:round(ag / count, 2);
+  local ab = ShaguScore:round(ab / count, 2);
+
+  if score ~= 0 then return score, ar, ag, ab else return nil end
+end
+
+function ShaguScore:GetItemLinkByName(name)
+  for itemID = 1, 25818 do
+    local itemName, hyperLink, itemQuality = GetItemInfo(itemID)
+    if (itemName and itemName == name) then
+      local _, _, _, hex = GetItemQualityColor(tonumber(itemQuality))
+      return hex.. "|H"..hyperLink.."|h["..itemName.."]|h|r"
+    end
+  end
+end
+
+-- hooks
+local ShaguScoreHookSetBagItem = GameTooltip.SetBagItem
+function GameTooltip.SetBagItem(self, container, slot)
+  GameTooltip.itemLink = GetContainerItemLink(container, slot)
+  _, GameTooltip.itemCount = GetContainerItemInfo(container, slot)
+  return ShaguScoreHookSetBagItem(self, container, slot)
+end
+
+local ShaguScoreHookSetQuestLogItem = GameTooltip.SetQuestLogItem
+function GameTooltip.SetQuestLogItem(self, itemType, index)
+  GameTooltip.itemLink = GetQuestLogItemLink(itemType, index)
+  if not GameTooltip.itemLink then return end
+  return ShaguScoreHookSetQuestLogItem(self, itemType, index)
+end
+
+local ShaguScoreHookSetQuestItem = GameTooltip.SetQuestItem
+function GameTooltip.SetQuestItem(self, itemType, index)
+  GameTooltip.itemLink = GetQuestItemLink(itemType, index)
+  return ShaguScoreHookSetQuestItem(self, itemType, index)
+end
+
+local ShaguScoreHookSetLootItem = GameTooltip.SetLootItem
+function GameTooltip.SetLootItem(self, slot)
+  GameTooltip.itemLink = GetLootSlotLink(slot)
+  ShaguScoreHookSetLootItem(self, slot)
+end
+
+local ShaguScoreHookSetInboxItem = GameTooltip.SetInboxItem
+function GameTooltip.SetInboxItem(self, mailID, attachmentIndex)
+  local itemName, itemTexture, inboxItemCount, inboxItemQuality = GetInboxItem(mailID)
+  GameTooltip.itemLink = ShaguScore:GetItemLinkByName(itemName)
+  return ShaguScoreHookSetInboxItem(self, mailID, attachmentIndex)
+end
+
+local ShaguScoreHookSetInventoryItem = GameTooltip.SetInventoryItem
+function GameTooltip.SetInventoryItem(self, unit, slot)
+  GameTooltip.itemLink = GetInventoryItemLink(unit, slot)
+  return ShaguScoreHookSetInventoryItem(self, unit, slot)
+end
+
+local ShaguScoreHookSetLootRollItem = GameTooltip.SetLootRollItem
+function GameTooltip.SetLootRollItem(self, id)
+  GameTooltip.itemLink = GetLootRollItemLink(id)
+  return ShaguScoreHookSetLootRollItem(self, id)
+end
+
+local ShaguScoreHookSetLootRollItem = GameTooltip.SetLootRollItem
+function GameTooltip.SetLootRollItem(self, id)
+  GameTooltip.itemLink = GetLootRollItemLink(id)
+  return ShaguScoreHookSetLootRollItem(self, id)
+end
+
+local ShaguScoreHookSetMerchantItem = GameTooltip.SetMerchantItem
+function GameTooltip.SetMerchantItem(self, merchantIndex)
+  GameTooltip.itemLink = GetMerchantItemLink(merchantIndex)
+  return ShaguScoreHookSetMerchantItem(self, merchantIndex)
+end
+
+local ShaguScoreHookSetCraftItem = GameTooltip.SetCraftItem
+function GameTooltip.SetCraftItem(self, skill, slot)
+  GameTooltip.itemLink = GetCraftReagentItemLink(skill, slot)
+  return ShaguScoreHookSetCraftItem(self, skill, slot)
+end
+
+local ShaguScoreHookSetCraftSpell = GameTooltip.SetCraftSpell
+function GameTooltip.SetCraftSpell(self, slot)
+  GameTooltip.itemLink = GetCraftItemLink(slot)
+  return ShaguScoreHookSetCraftSpell(self, slot)
+end
+
+local ShaguScoreHookSetTradeSkillItem = GameTooltip.SetTradeSkillItem
+function GameTooltip.SetTradeSkillItem(self, skillIndex, reagentIndex)
+  if reagentIndex then
+    GameTooltip.itemLink = GetTradeSkillReagentItemLink(skillIndex, reagentIndex)
+  else
+    GameTooltip.itemLink = GetTradeSkillItemLink(skillIndex)
+  end
+  return ShaguScoreHookSetTradeSkillItem(self, skillIndex, reagentIndex)
+end
+
+local HookSetAuctionItem = GameTooltip.SetAuctionItem
+function GameTooltip.SetAuctionItem(self, atype, index)
+  local itemName, _, itemCount = GetAuctionItemInfo(atype, index)
+  GameTooltip.itemCount = itemCount
+  GameTooltip.itemLink = GetItemLinkByName(itemName)
+  return HookSetAuctionItem(self, atype, index)
+end
+
+local ShaguScoreHookSetAuctionSellItem = GameTooltip.SetAuctionSellItem
+function GameTooltip.SetAuctionSellItem(self)
+  local itemName, _, itemCount = GetAuctionSellItemInfo()
+  GameTooltip.itemCount = itemCount
+  GameTooltip.itemLink = ShaguScore:GetItemLinkByName(itemName)
+  return ShaguScoreHookSetAuctionSellItem(self)
+end
+
+local ShaguScoreHookSetTradePlayerItem = GameTooltip.SetTradePlayerItem
+function GameTooltip.SetTradePlayerItem(self, index)
+  GameTooltip.itemLink = GetTradePlayerItemLink(index)
+  return ShaguScoreHookSetTradePlayerItem(self, index)
+end
+
+local ShaguScoreHookSetTradeTargetItem = GameTooltip.SetTradeTargetItem
+function GameTooltip.SetTradeTargetItem(self, index)
+  GameTooltip.itemLink = GetTradeTargetItemLink(index)
+  return ShaguScoreHookSetTradeTargetItem(self, index)
+end
+
+-- database
+ShaguScore.Database = {}
